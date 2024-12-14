12bb3ff9909fe4eedbb36d29d8512a1b551efa0f
vvshankarreddy <vvshankarreddy@gmail.com>
2024-12-14 10:16:35 +0530

    service update

--- email/emailService.js
+++ email/emailService.js
@@ -1,9 +1,13 @@
+require('dotenv').config(); // Ensure environment variables are loaded
+
 const { MailtrapClient } = require("mailtrap");
 const { mailtrapConfig } = require('../config/mailtrapConfig');
 const emailTemplate = require('./emailTemplate');
 
+// Initialize Mailtrap client
 const client = new MailtrapClient({ token: mailtrapConfig.TOKEN });
 
+// Function to send verification email
 async function sendVerificationEmail(email, name, verificationCode) {
   const subject = "Verify your email address";
   const text = emailTemplate.getVerificationText(name, verificationCode);
@@ -14,6 +18,8 @@ async function sendVerificationEmail(email, name, verificationCode) {
   };
 
   const recipients = [{ email }];
+  
+  // Send the email using Mailtrap
   await client.send({
     from: sender,
     to: recipients,
